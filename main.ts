248d6090e1f70b1a8985ba98245e33dcdb4e09ec
DehvaMCT <130194372+DehvaMCT@users.noreply.github.com>
2023-04-19 13:47:22 +0800

    Restore '15/04/2023, 4:19:16 pm Update main.ts'

--- main.ts
+++ main.ts
@@ -1,41 +1,41 @@
-enum NeoPixelLed {
+const enum NeoPixels {
     //% block="1"
-    L1 = 0,
+    neopixelled0,
     //% block="2"
-    L2 = 1,
+    neopixelled1,
     //% block="3"
-    L3 = 2,
+    neopixelled2,
     //% block="4"
-    L4 = 3,
+    neopixelled3,
     //% block="5"
-    L5 = 4,
+    neopixelled4,
     //% block="6"
-    L6 = 5,
+    neopixelled5,
     //% block="7"
-    L7 = 6,
+    neopixelled6,
     //% block="8"
-    L8 = 7
+    neopixelled7
 }
 
-enum Buttonpin {
+const enum Buttons {
     //% block="1"
-    B1,
+    button1,
     //% block="2"
-    B2
+    button2
 }
 
 //% color="#E4D00A"
 //% groups="['Sensor','Output']"
 namespace RekaCipta {
-    //% block="Button|%buttonchoice|pressed"
+    //% block="Button | %buttonchoice | pressed"
     //% group="Sensor"
     //% weight=4
-    export function ButtonPressed(buttonchoice: Buttonpin): boolean {
+    export function button(buttonchoice: Buttons): boolean {
         let buttonpin: DigitalPin;
-        if (buttonchoice == Buttonpin.B1) {
+        if (buttonchoice == Buttons.button1) {
             buttonpin = DigitalPin.P5;
         }
-        if (buttonchoice == Buttonpin.B2) {
+        if (buttonchoice == Buttons.button2) {
             buttonpin = DigitalPin.P11;
         }
         return pins.digitalReadPin(buttonpin) == 0;
@@ -44,7 +44,7 @@ namespace RekaCipta {
     //% block="Sound Sensor detected sound"
     //% group="Sensor"
     //% weight=3
-    export function SoundSensorDetectedSound(): boolean {
+    export function soundsensor(): boolean {
         let soundpin = DigitalPin.P0;
         return pins.digitalReadPin(soundpin) == 0;
     }
@@ -52,7 +52,7 @@ namespace RekaCipta {
     //% block="Potentiometer value"
     //% group="Sensor"
     //% weight=2
-    export function PotentiometerValue(): number {
+    export function potentiometer(): number {
         let potentiometerpin = AnalogPin.P1;
         return pins.analogReadPin(potentiometerpin);
     }
@@ -60,16 +60,16 @@ namespace RekaCipta {
     //% block="Infrared Sensor detected object"
     //% group="Sensor"
     //% weight=1
-    export function IRensorDetectedObject(): boolean {
+    export function irsensor(): boolean {
         let irpin = DigitalPin.P2;
         return pins.digitalReadPin(irpin) == 0;
     }
 
-    //% block="Buzzer tone|%tone"
+    //% block="Buzzer tone | %tone"
     //% tone.defl=100
     //% group="Output"
     //% weight=7
-    export function BuzzerTone(tone: number): void {
+    export function buzzertone(tone: number): void {
         let buzzerpin = AnalogPin.P16;
         pins.analogWritePin(buzzerpin, tone);
         pins.analogSetPeriod(buzzerpin, 500);
@@ -78,20 +78,16 @@ namespace RekaCipta {
     //% block="Buzzer Off"
     //% group="Output"
     //% weight=6
-    export function BuzzerOff(): void {
+    export function buzzeroff(): void {
         pins.analogWritePin(AnalogPin.P16, 0);
     }
 
-    //% block="NeoPixel LED:%neopixelChoice|Red:%red|Green:%green|Blue:%blue"
-    //% blockGap=10
-    //% weight=4
+    //% block="NeoPixel LED%neopixelChoice Red:%red Green:%green Blue:%blue"
     //% red.defl=255
     //% green.defl=255
-    //% blue.defl=255
-    //% group="Output"
-    export function NeoPixel(neopixelChoice: NeoPixelLed, red: number, green: number, blue: number): void {
-        let NeoPixelpin = neopixel.create(DigitalPin.P8, 8, NeoPixelMode.RGB);
-        NeoPixelpin.range(neopixelChoice, 1).showColor(neopixel.rgb(red, green, blue))
-        //NeoPixelpin.range(neopixelChoice, 1).show()
+    //% blue.defl=255 
+    //% weight=4
+    export function neopixel(neopixelChoice: NeoPixels, red: number, green: number, blue: number): void {
+
     }
 }
