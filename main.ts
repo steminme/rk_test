bf8124ad640d5f87309f9bff2d0df948a2ccc828
DehvaMCT <130194372+DehvaMCT@users.noreply.github.com>
2023-04-19 13:47:00 +0800

    Update pxt.json, main.ts

--- main.ts
+++ main.ts
@@ -1,43 +1,27 @@
-const enum NeoPixels {
+enum NeoPixelLed {
     //% block="1"
-    led1 = 0,
+    L1 = 0,
     //% block="2"
-    led2 = 1,
+    L2 = 1,
     //% block="3"
-    led3 = 2,
+    L3 = 2,
     //% block="4"
-    led4 = 3,
+    L4 = 3,
     //% block="5"
-    led5 = 4,
+    L5 = 4,
     //% block="6"
-    led6 = 5,
+    L6 = 5,
     //% block="7"
-    led7 = 6,
+    L7 = 6,
     //% block="8"
-    led8 = 7
+    L8 = 7
 }
 
-const enum Buttons {
+enum Buttonpin {
     //% block="1"
-    button1,
+    B1,
     //% block="2"
-    button2
-}
-
-namespace ws2812b {
-    //% shim=sendBufferAsm
-    export function sendBuffer(buf: Buffer, pin: DigitalPin) {
-    }
-
-    //% shim=setBufferMode
-    export function setBufferMode(pin: DigitalPin, mode: number) {
-
-    }
-
-    export const BUFFER_MODE_RGB = 1
-    export const BUFFER_MODE_RGBW = 2
-    export const BUFFER_MODE_RGB_RGB = 3
-    export const BUFFER_MODE_AP102 = 4
+    B2
 }
 
 //% color="#E4D00A"
@@ -46,12 +30,12 @@ namespace RekaCipta {
     //% block="Button|%buttonchoice|pressed"
     //% group="Sensor"
     //% weight=4
-    export function button(buttonchoice: Buttons): boolean {
+    export function ButtonPressed(buttonchoice: Buttonpin): boolean {
         let buttonpin: DigitalPin;
-        if (buttonchoice == Buttons.button1) {
+        if (buttonchoice == Buttonpin.B1) {
             buttonpin = DigitalPin.P5;
         }
-        if (buttonchoice == Buttons.button2) {
+        if (buttonchoice == Buttonpin.B2) {
             buttonpin = DigitalPin.P11;
         }
         return pins.digitalReadPin(buttonpin) == 0;
@@ -60,7 +44,7 @@ namespace RekaCipta {
     //% block="Sound Sensor detected sound"
     //% group="Sensor"
     //% weight=3
-    export function soundsensor(): boolean {
+    export function SoundSensorDetectedSound(): boolean {
         let soundpin = DigitalPin.P0;
         return pins.digitalReadPin(soundpin) == 0;
     }
@@ -68,7 +52,7 @@ namespace RekaCipta {
     //% block="Potentiometer value"
     //% group="Sensor"
     //% weight=2
-    export function potentiometer(): number {
+    export function PotentiometerValue(): number {
         let potentiometerpin = AnalogPin.P1;
         return pins.analogReadPin(potentiometerpin);
     }
@@ -76,7 +60,7 @@ namespace RekaCipta {
     //% block="Infrared Sensor detected object"
     //% group="Sensor"
     //% weight=1
-    export function irsensor(): boolean {
+    export function IRensorDetectedObject(): boolean {
         let irpin = DigitalPin.P2;
         return pins.digitalReadPin(irpin) == 0;
     }
@@ -85,7 +69,7 @@ namespace RekaCipta {
     //% tone.defl=100
     //% group="Output"
     //% weight=7
-    export function buzzertone(tone: number): void {
+    export function BuzzerTone(tone: number): void {
         let buzzerpin = AnalogPin.P16;
         pins.analogWritePin(buzzerpin, tone);
         pins.analogSetPeriod(buzzerpin, 500);
@@ -94,24 +78,20 @@ namespace RekaCipta {
     //% block="Buzzer Off"
     //% group="Output"
     //% weight=6
-    export function buzzeroff(): void {
+    export function BuzzerOff(): void {
         pins.analogWritePin(AnalogPin.P16, 0);
     }
 
     //% block="NeoPixel LED:%neopixelChoice|Red:%red|Green:%green|Blue:%blue"
+    //% blockGap=10
+    //% weight=4
     //% red.defl=255
     //% green.defl=255
     //% blue.defl=255
     //% group="Output"
-    //% weight=4
-    export function neopixel(neopixelChoice: NeoPixels, red: number, green: number, blue: number): void {
-        let start = neopixelChoice * 3
-        let colors = pins.createBuffer(24)
-        for (let i = 0; i < 24; i += 3) {
-            colors[i + 0] = (i == start) ? green : 0
-            colors[i + 1] = (i == start + 1) ? red : 0
-            colors[i + 2] = (i == start + 2) ? blue : 0
-        }
-        ws2812b.sendBuffer(colors, DigitalPin.P8)
+    export function NeoPixel(neopixelChoice: NeoPixelLed, red: number, green: number, blue: number): void {
+        let NeoPixelpin = neopixel.create(DigitalPin.P8, 8, NeoPixelMode.RGB);
+        NeoPixelpin.range(neopixelChoice, 1).showColor(neopixel.rgb(red, green, blue))
+        //NeoPixelpin.range(neopixelChoice, 1).show()
     }
 }
